b94419cd19155e29b05a230e0ee5f7b303ca69ab
Yu-phys <saitoy0722@gmail.com>
2022-12-22 03:01:24 +0900

    change date to 1900-01-01 and add everything

--- colabfold/batch.py
+++ colabfold/batch.py
@@ -151,7 +151,7 @@ def mk_template(
 ) -> Dict[str, Any]:
     template_featurizer = templates.HhsearchHitFeaturizer(
         mmcif_dir=template_path,
-        max_template_date="2022-09-09",
+        max_template_date="1900-01-02",
         max_hits=20,
         kalign_binary_path="kalign",
         release_dates_path=None,
